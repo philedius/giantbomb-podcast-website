97c79b68e10c4f137637cc07272f9695dd38f5c1
philedius <fannarr@gmail.com>
2016-10-27 22:40:47 +0000

    fixing port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 app.use(express.static('public'));
 app.use('/scripts', express.static(__dirname));
-
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
     res.render('index', {data: data});
 });
 
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
     console.log('Example app listening on port 3000!');
 });
